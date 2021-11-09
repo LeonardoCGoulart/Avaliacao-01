2188e493a6a43f56bfa43fb85d80d0ed020abdb2
LeonardoCGoulart <joedarkalone@hotmail.com>
2021-11-09 13:20:27 -0300

    valor menor que 2000 falhando

--- test/main.test.js
+++ test/main.test.js
@@ -1,9 +1,32 @@
 const GerenciaFatura = require("../lib/GerenciaFatura");
+const Cliente = require("../lib/Cliente");
+const Fatura = require("../lib/Fatura");
 describe('GerenciaFatura', () => {
     test('teste commit inicial', () => {
         fatura = new GerenciaFatura();
         const valor = fatura.testeSoma(1,2);
         expect(valor).toBe(3); 
+    });
+
+    test('teste fatura', () => {
+        cliente = new Cliente();
+        fatura = new Fatura();
+        
+        const cliente1 = cliente.criarCliente('leo', '1/1/1', 'pr');
+        const cliente2 = cliente.criarCliente('joao', '2/2/2', 'sp');
+
+        const fatura1 = fatura.criarFatura('codigo', 100, '1/1/1', cliente1);
+        const fatura2 = fatura.criarFatura('codigo2', 1000, '2/2/2', cliente2);
+
+        let arr = []
+        arr = fatura.addFaturaToArray(arr,fatura1)
+        arr = fatura.addFaturaToArray(arr,fatura2)
+        
+        filtroFatura = new GerenciaFatura();
+        arr = filtroFatura.filtroFatura(arr);
+
+        const tamanhoArray = arr.length;
+        expect(tamanhoArray).toBe(0); 
     });
 
    
