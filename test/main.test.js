aadbd0bb77bac04276aed3925332f1f0831eae5c
LeonardoCGoulart <joedarkalone@hotmail.com>
2021-11-10 14:36:36 -0300

    valor maior que 4000 e cliente sul do Brasil - refatorado

--- test/main.test.js
+++ test/main.test.js
@@ -97,5 +97,26 @@ describe('GerenciaFatura', () => {
         expect(tamanhoArray).toBe(0); 
     });
 
+    test('teste fatura - valor maior que 4000 e cliente da região sul do Brasil II', () => {
+        cliente = new Cliente();
+        fatura = new Fatura();
+        
+        const cliente1 = cliente.criarCliente('leo', '2021-11-01', 'pr'); // fatura > 4000 e cliente do sul, a fatura deve ser apagada
+        const fatura1 = fatura.criarFatura('codigo', 4001, '2021-11-01', cliente1);
+
+        const cliente2 = cliente.criarCliente('joao', '2021-11-01', 'sp'); // fatura > 4000 e cliente de SP, a fatura nao deve ser apagada
+        const fatura2 = fatura.criarFatura('codigo2', 4001, '2021-11-01', cliente2);
+
+        let arr = []
+        arr = fatura.addFaturaToArray(arr,fatura1)
+        arr = fatura.addFaturaToArray(arr,fatura2)
+        
+        filtroFatura = new GerenciaFatura();
+        arr = filtroFatura.filtroFatura(arr);
+
+        const tamanhoArray = arr.length;
+        expect(tamanhoArray).toBe(1); 
+    });
+
    
 });
