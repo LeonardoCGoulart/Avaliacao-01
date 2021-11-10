630799f79882059d41d4e429ee4045dce2aa86b4
LeonardoCGoulart <joedarkalone@hotmail.com>
2021-11-10 14:04:18 -0300

    valor entre 2500 - 3000 e data cliente <= 2 meses - refatorado

--- test/main.test.js
+++ test/main.test.js
@@ -2,11 +2,6 @@ const GerenciaFatura = require("../lib/GerenciaFatura");
 const Cliente = require("../lib/Cliente");
 const Fatura = require("../lib/Fatura");
 describe('GerenciaFatura', () => {
-    test('teste commit inicial', () => {
-        fatura = new GerenciaFatura();
-        const valor = fatura.testeSoma(1,2);
-        expect(valor).toBe(3); 
-    });
 
     test('teste fatura - menor que 2000', () => {
         cliente = new Cliente();
@@ -62,6 +57,27 @@ describe('GerenciaFatura', () => {
 
         const tamanhoArray = arr.length;
         expect(tamanhoArray).toBe(0); 
+    });
+
+    test('teste fatura - valor entre 2500 - 3000 e data cliente <= 2 meses - II', () => {
+        cliente = new Cliente();
+        fatura = new Fatura();
+        
+        const cliente1 = cliente.criarCliente('leo', '2021-11-01', 'pr'); // cliente data < 2 meses logo a fatura deve ser apagada
+        const fatura1 = fatura.criarFatura('codigo', 3000, '2021-11-01', cliente1);
+
+        const cliente2 = cliente.criarCliente('joao', '2021-08-01', 'pr'); // cliente data > 2 meses logo a fatura não deve ser apagada
+        const fatura2 = fatura.criarFatura('codigo2', 3500, '2021-11-01', cliente2);
+
+        let arr = []
+        arr = fatura.addFaturaToArray(arr,fatura1)
+        arr = fatura.addFaturaToArray(arr,fatura2)
+        
+        filtroFatura = new GerenciaFatura();
+        arr = filtroFatura.filtroFatura(arr);
+
+        const tamanhoArray = arr.length;
+        expect(tamanhoArray).toBe(1); 
     });
 
    
